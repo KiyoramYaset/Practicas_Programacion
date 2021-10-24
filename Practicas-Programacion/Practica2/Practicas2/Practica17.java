f7bab550133e6aa84a8b4fc73ae8a3c21822454e
KiyoramYaset <91904051+KiyoramYaset@users.noreply.github.com>
2021-10-24 21:39:12 +0200

    Create Practica17
    24/10/21

---
+++ Practicas-Programacion/Practica2/Practicas2/Practica17.java
@@ -0,0 +1,20 @@
+package Practicas2;
+import java.util.Scanner;
+
+public class Practica17 {
+	public static void main(String [] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Indica el tamaño del cuadrado");
+		
+		int n = keyboard.nextInt(); // Se le indica a la variable que se le intrudicira un elemento
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
