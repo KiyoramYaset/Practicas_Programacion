0ffeaf699bb5c5adaf7e3d8ac26722a9f259f29f
KiyoramYaset <91904051+KiyoramYaset@users.noreply.github.com>
2021-10-28 12:01:25 +0200

    Pruebas

---
+++ Practicas-Programacion/Practica2/Practicas2/PosibleExamen.java
@@ -0,0 +1,21 @@
+package Practicas2;
+import java.util.Scanner;
+
+public class PosibleExamen {
+	public static void main (String[]args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		long teclado = keyboard.nextLong();
+		
+		System.out.println(teclado);
+		
+		
+		
+	
+	
+		
+		keyboard.close();
+		
+	}
+
+}
